52ceedd4e42e1e4c0bb73f48a210445447cd2e2f
Jon Winton <winton.jon@gmail.com>
2018-01-25 13:08:37 -0500

    first commit

---
+++ test/index.js
@@ -0,0 +1,22 @@
+'use strict';
+const glob = require('glob'),
+  _ = require('lodash'),
+  chai = require('chai'),
+  path = require('path'),
+  tests = glob.sync([__dirname, '..', 'services', '**', '*.test.js'].join(path.sep));
+
+// defaults for chai
+chai.config.showDiff = true;
+chai.config.truncateThreshold = 0;
+
+// Right now we only have the index file
+require('../index.test.js');
+
+_.each(tests, function (test) {
+  require(test);
+});
+
+// process.on('unhandledRejection', (reason, p) => {
+//   console.log('Unhandled Rejection at:', p, 'reason:', reason);
+//   // application specific logging, throwing an error, or other logic here
+// });
