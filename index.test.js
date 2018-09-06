e94d54cc092fbfc4712e488a7b1fbdf7a31fbc57
Yuliy Vigdorchik <yuliy.vigdorchik@nymag.com>
2018-09-06 16:28:29 -0400

    Convert from mocha/sinon/chai to jest

--- index.test.js
+++ index.test.js
@@ -3,40 +3,27 @@
 const _ = require('lodash'),
   filename = __filename.split('/').pop().split('.').shift(),
   lib = require('./' + filename),
-  { expect } = require('chai'),
-  sinon = require('sinon');
+  fakeLog = () => false;
 
-describe(_.startCase(filename), function () {
-  let sandbox, logSpy;
-
-  beforeEach(function () {
-    sandbox = sinon.sandbox.create();
-    logSpy = sandbox.spy();
-
-    lib.setLog(logSpy);
-  });
-
-  afterEach(function () {
-    sandbox.restore();
+describe(_.startCase(filename), () => {
+  beforeEach(() => {
+    lib.setLog(fakeLog);
   });
 
   function makeFakeRes() {
     return {
-      type: sandbox.spy(),
-      send: sandbox.spy(),
-      status: sandbox.spy(),
-      json: sandbox.spy()
+      type: () => false,
+      send: () => false,
+      status: () => false,
+      json: () => false
     };
   }
 
-  describe('render', function () {
-    const fn = lib[this.title];
+  describe('render', () => {
+    const fn = lib['render'];
 
-    it('returns an object with `output` and `type` properties', function () {
-      const fakeRes = {
-          type: sandbox.spy(),
-          send: sandbox.spy()
-        },
+    test('returns an object with `output` and `type` properties', () => {
+      const res = makeFakeRes(),
         result = fn({
           feed: [],
           meta: {
@@ -44,107 +31,132 @@ describe(_.startCase(filename), function () {
             description: 'bar',
             link: 'foobar'
           }
-        }, {}, fakeRes);
+        }, {}, res);
+
+      jest.spyOn(res, 'type');
+      jest.spyOn(res, 'send');
 
       return result.then(function () {
-        sinon.assert.calledWith(fakeRes.type, 'text/rss+xml');
-        sinon.assert.calledOnce(fakeRes.send);
+        expect(res.type.mock.calls[0][0]).toBe('text/rss+xml');
+        expect(res.send.mock.calls.length).toBe(1);
       });
     });
 
-    it('works', function () {
+    test('works', () => {
       const res = makeFakeRes(),
         result = fn({
           feed: [],
           meta: {}
         }, {}, res);
 
+      jest.spyOn(res, 'json');
 
-      result.then(function () {
-        sinon.assert.calledWith(res.json, {status: 500, message: 'No data send to XML renderer, cannot respond'});
+      return result.then(function () {
+        expect(res.json.mock.calls[0][0]).toEqual({status: 500, message: 'No data send to XML renderer, cannot respond'});
       });
     });
   });
 
-  describe('wrapInItem', function () {
-    const fn = lib[this.title];
+  describe('wrapInItem', () => {
+    const fn = lib['wrapInItem'];
 
-    it('returns a passed in value as the value for an `item` property', function () {
-      sinon.assert.match(fn('foo'), { item: 'foo' });
-    });
+    test(
+      'returns a passed in value as the value for an `item` property',
+      () => {
+        expect(fn('foo')).toEqual({ item: 'foo' });
+      }
+    );
   });
 
-  describe('wrapInTopLevel', function () {
-    const fn = lib[this.title],
+  describe('wrapInTopLevel', () => {
+    const fn = lib['wrapInTopLevel'],
       val = fn('foo'),
       customAttr = {bar: 'http://bar.com'},
       valWithAttr = fn('foo', customAttr);
 
-    it('wraps the passed in value in an object with and rss array', function () {
-      expect(Array.isArray(val.rss)).to.be.true;
+    test('wraps the passed in value in an object with and rss array', () => {
+      expect(Array.isArray(val.rss)).toBe(true);
     });
 
-    it('has an object with an `_attr` property as the first object of the `rss` Array', function () {
-      expect(val.rss[0]._attr).to.not.be.undefined;
-    });
+    test(
+      'has an object with an `_attr` property as the first object of the `rss` Array',
+      () => {
+        expect(val.rss[0]._attr).toBeDefined();
+      }
+    );
 
-    it('merges attr instance data into the default _attr properties', function () {
-      expect(valWithAttr.rss[0]._attr).to.have.property('bar', 'http://bar.com');
-    });
+    test(
+      'merges attr instance data into the default _attr properties',
+      () => {
+        expect(valWithAttr.rss[0]._attr).toHaveProperty('bar', 'http://bar.com');
+      }
+    );
 
-    it('has an object with a `channel` property as the second object of the `rss` Array', function () {
-      expect(val.rss[1].channel).to.not.be.undefined;
-    });
+    test(
+      'has an object with a `channel` property as the second object of the `rss` Array',
+      () => {
+        expect(val.rss[1].channel).toBeDefined();
+      }
+    );
 
-    it('the value passed into the function is assigned to the `channel` property', function () {
-      expect(val.rss[1].channel).to.eql('foo');
-    });
+    test(
+      'the value passed into the function is assigned to the `channel` property',
+      () => {
+        expect(val.rss[1].channel).toEqual('foo');
+      }
+    );
   });
 
-  describe('feedMetaTags', function () {
-    const fn = lib[this.title];
+  describe('feedMetaTags', () => {
+    const fn = lib['feedMetaTags'];
 
-    it('returns a function', function () {
+    test('returns a function', () => {
       const result = fn({title:'foo', description: 'bar', link: 'foobar'});
 
-      expect(result).to.be.an('function');
+      expect(typeof result).toBe('function');
     });
 
-    it('its callback returns an array', function () {
+    test('its callback returns an array', () => {
       const result = fn({title:'foo', description: 'bar', link: 'foobar'});
 
-      expect(result([])).to.be.an('array');
+      expect(Array.isArray(result([]))).toBe(true);
     });
 
-    it('its callback assigns passed in values to the return array', function () {
+    test('its callback assigns passed in values to the return array', () => {
       const [ one, two, three ] = fn({title:'foo', description: 'bar', link: 'foobar'})([]);
 
-      expect(one.title).to.eql('foo');
-      expect(two.description).to.eql('bar');
-      expect(three.link).to.eql('foobar');
+      expect(one.title).toEqual('foo');
+      expect(two.description).toEqual('bar');
+      expect(three.link).toEqual('foobar');
     });
 
-    it('accepts an `opt` object with additional meta tags', function () {
+    test('accepts an `opt` object with additional meta tags', () => {
       const language = {language: 'en-US'},
         result = fn({title:'foo', description: 'bar', link: 'foobar', opt: language})([]);
 
-      expect(result).have.to.deep.include(language);
+      expect(result).toContain(language);
     });
   });
 
-  describe('elevateCategory', function () {
-    const fn = lib[this.title];
+  describe('elevateCategory', () => {
+    const fn = lib['elevateCategory'];
 
-    it('returns strings of category tags from each item\'s categories', function () {
-      var result = fn([{item: [{ category: 'foo'}, { category: 'bar'}]}]);
+    test(
+      'returns strings of category tags from each item\'s categories',
+      () => {
+        const result = fn([{item: [{ category: 'foo'}, { category: 'bar'}]}]);
 
-      expect(result).to.eql([ { category: 'foo,bar' } ]);
-    });
+        expect(result).toEqual([ { category: 'foo,bar' } ]);
+      }
+    );
 
-    it('returns an empty array if none of the items have categories', function () {
-      var result = fn([{item: [{ notCategory: 'foo'}, { notCategory: 'bar'}]}]);
+    test(
+      'returns an empty array if none of the items have categories',
+      () => {
+        const result = fn([{item: [{ notCategory: 'foo'}, { notCategory: 'bar'}]}]);
 
-      expect(result).to.eql([]);
-    });
+        expect(result).toEqual([]);
+      }
+    );
   });
 });
